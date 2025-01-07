d26a726171067a995426427ad9b35929f395ea5f
maymayy08 <170673618+maymayy08@users.noreply.github.com>
2025-01-07 18:02:59 +0000

    deck.py

--- segs-2024-tech-test-candidate-pack/segs-blackjack-starter/src/deck.py
+++ segs-2024-tech-test-candidate-pack/segs-blackjack-starter/src/deck.py
@@ -3,36 +3,36 @@ class Deck:
         # Initialize the cards with suits and face cards including Aces
         self.suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'] 
         self.face_cards = ['Jack', 'Queen', 'King']
-        self.values = [2, 3, 4, 5, 6, 7, 8, 9, 10]  # Numeric cards (between 2-10)
+        self.values = [2, 3, 4, 5, 6, 7, 8, 9, 10]  # Numeric cards (between 2-10).
         self.aces = ['Ace']
 
         # Create full set deck of cards
-        self.cards = [(value, suit) for value in self.values for suit in self.suits]  # Numeric cards
-        self.cards += [(face_card, suit) for face_card in self.face_cards for suit in self.suits]  # Face cards
+        self.cards = [(value, suit) for value in self.values for suit in self.suits]  # Numeric cards.
+        self.cards += [(face_card, suit) for face_card in self.face_cards for suit in self.suits]  # Face cards.
         self.cards += [(ace, suit) for ace in self.aces for suit in self.suits]  # Ace cards
 
     def obtain_the_deck_value(self, card):
-        # Returns the value for each single card
+        # Returns the value for each single card.
         value, suit = card
         (print(card))
 
-        # This method is use to check For numeric cards (2-10)
+        # This method is use to check For numeric cards (2-10).
         if value.isdigit():
             return int(value)
 
-        # This method is use to check the Face cards (Jack, Queen, King) that is equal to 10 points
+        # This method is use to check the Face cards (Jack, Queen, King) that is equal to 10 points.
         if value in ["Jack", "Queen", "King"]:
             return 10
 
-        # This method is use to check if Ace is equal to 1 (or 11, adjusted in hand calculations)
+        # This method is use to check if Ace is equal to 1 (or 11, adjusted in hand calculations).
         if value == "Ace":
             return 1
         
-        # This method is use to return the value back without indicating any error found
+        # This method is use to return the value back without indicating any error found.
         return 0
 
     def count_aces(self):
-        """Returns the number of Aces in the deck."""
+        # Returns the number of Aces in the deck.
         ace_count = 0
         for card in self.cards:
             value, suit = card
